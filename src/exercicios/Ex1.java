f2d247789cbcee55986c3399e43eeac2e5a6f142
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-22 21:30:04 -0300

    Ex1

--- src/exercicios/Ex1.java
+++ src/exercicios/Ex1.java
@@ -1,4 +1,18 @@
 package exercicios;
+import java.util.Scanner;
 
 public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        int antecessor = numero - 1;
+        int sucessor = numero + 1;
+
+        System.out.printf("Antecessor: %d Sucessor: %d ", antecessor,sucessor);
+
+        scanner.close();
+    }
 }
---
+++ src/exercicios/Ex3.java
@@ -0,0 +1,21 @@
+package exercicios;
+import javax.swing.*;
+
+public class Ex2 {
+    public static void main(String[] args) {
+       String entrada = JOptionPane.showInputDialog("Digite um número inteiro: ");
+
+           if (entrada == null || entrada.isEmpty()) {
+               System.exit(0);
+           }
+
+           int numero = Integer.parseInt(entrada);
+
+           int antecessor = numero - 1;
+           int sucessor = numero + 1;
+
+           String m = String.format("Antecessor: %d Sucessor: %d ", antecessor, sucessor);
+           JOptionPane.showMessageDialog(null, m);
+           }
+
+    }
