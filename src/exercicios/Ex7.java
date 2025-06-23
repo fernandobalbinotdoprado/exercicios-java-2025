2a9203b835c1fe03a7a8dd2d7eb3cb14d5c4f428
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-22 21:31:54 -0300

    Ex7

--- src/exercicios/Ex7.java
+++ src/exercicios/Ex7.java
@@ -1,4 +1,31 @@
 package exercicios;
 
+import javax.swing.*;
+
 public class Ex7 {
+    public static void main(String[] args) {
+        String entrada = JOptionPane.showInputDialog("Quanto você ganha por hora?");
+
+        if (entrada == null || entrada.isEmpty()) {
+            System.exit(0);
+        }
+
+        float ganho = Integer.parseInt(entrada);
+
+        String entrada2 = JOptionPane.showInputDialog("Numero de horas trabalhadas no mês?: ");
+
+        if (entrada2 == null || entrada2.isEmpty()) {
+            System.exit(0);
+        }
+        float horas = Integer.parseInt(entrada2);
+
+        float salario = ganho * horas;
+
+        String m = String.format("Ganho por hora: %.2f \nNumero de horas trabalhadas no mês: %.2f \n Salário: %.2f",ganho, horas,salario);
+        JOptionPane.showMessageDialog(null, m);
+
+
+
+
+    }
 }
