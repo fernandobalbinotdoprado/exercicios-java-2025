8f0659e88ee6b4a07c7d3e7aead04c9b17041834
Fernando Balbinot do Prado <skullexteminator@gmail.com>
2025-06-22 21:31:37 -0300

    Ex5

--- src/exercicios/Ex5.java
+++ src/exercicios/Ex5.java
@@ -1,4 +1,18 @@
 package exercicios;
+import java.util.Scanner;
 
 public class Ex5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quanto você ganha por hora?: ");
+        float ganho = scanner.nextInt();
+        System.out.print("Numero de horas trabalhadas no mês?: ");
+        float numerohoras = scanner.nextInt();
+
+        System.out.printf("Ganho por hora: %.2f \nNumero de horas trabalhadas no mês: %.2f \n", ganho, numerohoras);
+
+        float calculosalario = ganho * numerohoras;
+        System.out.printf("O seu salário total é: %.2f ", calculosalario);
+    }
 }
